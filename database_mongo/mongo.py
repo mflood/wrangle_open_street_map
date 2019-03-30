1258b1f4bf9a82bcaa9b2d245e29a7835d9842d1
Matthew Flood <hermannwest@gmail.com>
2019-03-30 10:55:18 -0600

    add more mongo files

---
+++ database_mongo/mongo.py
@@ -0,0 +1,24 @@
+
+def add_city(db):
+    # Changes to this function will be reflected in the output. 
+    # All other functions are for local use only.
+    # Try changing the name of the city to be inserted
+    db.cities.insert({"name" : "Boulder"})
+    
+def get_city(db):
+    print(dir(db.cities))
+    return db.cities.find()
+
+def get_db():
+    # For local use
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    # 'examples' here is the database name. It will be created if it does not exist.
+    db = client.examples
+    return db
+
+if __name__ == "__main__":
+    # For local use
+    db = get_db() # uncomment this line if you want to run this locally
+    add_city(db)
+    print(list(get_city(db)))
